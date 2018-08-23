9e3791397a2f75ca0c7ba541b9da9d98f148113f
kapoorlab <randomaccessiblekapoor@gmail.com>
2018-08-24 01:40:00 +0200

    Overlap display

--- utils/DiscreteClustering.py
+++ utils/DiscreteClustering.py
@@ -2,13 +2,13 @@ from sklearn.feature_extraction import image as imp
 import numpy as np
 import time
 from sklearn.cluster import spectral_clustering
-def Discretize_Clustering(twoDimg):
+def Discretize_Clustering(twoDimg,N_REGIONS ):
       """Put clustering code"""
       graph = imp.img_to_graph(twoDimg)
       beta = 5
       eps = 1e-6
       graph.data = np.exp(-beta * graph.data / twoDimg.std()) + eps 
-      N_REGIONS = 100
+      
     
       t0 = time.time()
       labels = spectral_clustering(graph, n_clusters=N_REGIONS,
